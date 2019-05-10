2bf16024b45af1b29ccb9464042150ff114b79e0
Naxess <30292137+Naxesss@users.noreply.github.com>
2019-05-10 12:35:48 +0200

    Add private constructor to Renderer

--- renderer/Renderer.cs
+++ renderer/Renderer.cs
@@ -11,6 +11,8 @@ namespace MapsetVerifierApp.renderer
 {
     public class Renderer
     {
+        private Renderer() { }
+
         /// <summary> Surrounds the content with a div tag using the given class(es), as well as any other attributes in the tag, like id or data. </summary>
         protected static string DivAttr(string aClass, string anAttr, params object[] aContents)
         {
