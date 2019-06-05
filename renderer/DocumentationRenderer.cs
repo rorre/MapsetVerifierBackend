7cd2d5862ab4f4be14fb821911ecb238e1ba37a9
Naxess <30292137+Naxesss@users.noreply.github.com>
2019-06-05 19:55:35 +0200

    Change documentation design

--- renderer/DocumentationRenderer.cs
+++ renderer/DocumentationRenderer.cs
@@ -63,7 +63,7 @@ namespace MapsetVerifierApp.renderer
             return
                 Div("doc-mode-title", title) +
                 Div("doc-mode-content",
-                    Div("doc-box-container",
+                    Div("doc-mode-inner",
                         aChecks.OrderByDescending(aCheck => aCheck.GetMetadata().Category).Select(RenderCheckBox).ToArray()
                     )
                 );
@@ -76,23 +76,13 @@ namespace MapsetVerifierApp.renderer
                 RenderDocBox(
                     "check",
                     Encode(aCheck.GetMetadata().Message),
+                    Encode(aCheck.GetMetadata().GetMode() + " > " + aCheck.GetMetadata().Category),
 
                     String.Concat(
-                    aCheck.GetTemplates().Select(aPair => aPair.Value).Select(aTemplate =>
-                    {
-                        return
-                            Div("card-detail-icon " + GetIcon(aTemplate.Level) + "-icon") +
-                            Div("doc-box-issue",
-                                aTemplate.ToString()
-                            );
-                    })),
-
-                    Div("doc-box-category",
-                        Encode(aCheck.GetMetadata().GetMode() + " > " + aCheck.GetMetadata().Category)
-                    ) +
-                    Div("doc-box-author",
-                        Encode(aCheck.GetMetadata().Author)
-                    )
+                        aCheck.GetTemplates().Select(aPair => aPair.Value).Select(aTemplate =>
+                            Div("card-detail-icon " + GetIcon(aTemplate.Level) + "-icon"))),
+                    
+                    Encode(aCheck.GetMetadata().Author)
                 );
         }
 
@@ -102,37 +92,67 @@ namespace MapsetVerifierApp.renderer
                 Div("doc-mode-title",
                     "Icons"
                 ) +
-                Div("doc-box-container",
-                    RenderDocBox("check",          "Check",    "No issues were found.",                                "Checks", true),
-                    RenderDocBox("error",          "Error",    "An error occurred preventing a complete check.",       "Checks", true),
-                    RenderDocBox("minor",          "Minor",    "One or more negligible issues may have been found.",   "Checks", true),
-                    RenderDocBox("exclamation",    "Warning",  "One or more issues may have been found.",              "Checks", true),
-                    RenderDocBox("cross",          "Problem",  "One or more issues were found.",                       "Checks", true),
+                Div("doc-mode-inner doc-mode-icons",
+                    RenderIconsDocBox("check",          "Check",    "Checks", "No issues were found."),
+                    RenderIconsDocBox("error",          "Error",    "Checks", "An error occurred preventing a complete check."),
+                    RenderIconsDocBox("minor",          "Minor",    "Checks", "One or more negligible issues may have been found."),
+                    RenderIconsDocBox("exclamation",    "Warning",  "Checks", "One or more issues may have been found."),
+                    RenderIconsDocBox("cross",          "Problem",  "Checks", "One or more issues were found."),
 
-                    RenderDocBox("gear-gray",  "None",     "No changes were made.",            "Snapshots", true),
-                    RenderDocBox("minus",      "Removal",  "One or more lines were removed.",  "Snapshots", true),
-                    RenderDocBox("plus",       "Addition", "One or more lines were added.",    "Snapshots", true),
-                    RenderDocBox("gear-blue",  "Change",   "One or more lines were changed.",  "Snapshots", true)
+                    RenderIconsDocBox("gear-gray",  "None",     "Snapshots", "No changes were made."),
+                    RenderIconsDocBox("minus",      "Removal",  "Snapshots", "One or more lines were removed."),
+                    RenderIconsDocBox("plus",       "Addition", "Snapshots", "One or more lines were added."),
+                    RenderIconsDocBox("gear-blue",  "Change",   "Snapshots", "One or more lines were changed.")
                 );
         }
 
-        public static string RenderDocBox(string anIcon, string aTitle, string aDesc, string aCategory, bool aIconDoc = false)
+        public static string RenderDocBox(string anIcon, string aTitle, string aSubtitle, string anIssueOverview = null, string anAuthor = null)
         {
             return
-                Div((aIconDoc ? "doc-icon-box" : "doc-box"),
-                    Div("doc-box-inner",
+                Div("doc-box-container",
+                    Div("doc-box-left",
+                        Div("doc-box-icon-container",
+                            Div("doc-box-icon " + anIcon + "-icon")
+                        ),
                         Div("doc-box-content",
                             Div("doc-box-title",
-                                Div("doc-box-icon " + anIcon + "-icon"),
                                 aTitle
                             ),
-                            Div("doc-box-desc",
-                                aDesc
+                            Div("doc-box-subtitle",
+                                aSubtitle
                             )
-                        ),
-                        Div("doc-box-footer",
-                            aCategory
                         )
+                    ),
+                    Div("doc-box-right",
+                        Div("doc-box-right-upper",
+                            anIssueOverview
+                        ),
+                        Div("doc-box-right-lower",
+                            anAuthor
+                        )
+                    )
+                );
+        }
+
+        public static string RenderIconsDocBox(string anIcon, string aTitle, string aSubtitle, string aDescription)
+        {
+            return
+                Div("doc-box-container",
+                    Div("doc-box-left",
+                        Div("doc-box-icon-container",
+                            Div("doc-box-icon " + anIcon + "-icon")
+                        ),
+                        Div("doc-box-content",
+                            Div("doc-box-title",
+                                aTitle
+                            ),
+                            Div("doc-box-subtitle",
+                                aSubtitle
+                            )
+                        )
+                    ),
+                    Div("doc-box-right",
+                        aDescription
                     )
                 );
         }
