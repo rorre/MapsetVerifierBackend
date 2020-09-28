faa21718395bb3b7da02f014db07663fc7c9dba4
Rendy Arya Kemal <rendyarya22@gmail.com>
2020-09-28 11:49:18 +0700

    Refactor code for easier understanding

--- renderer/ChartRenderer.cs
+++ renderer/ChartRenderer.cs
@@ -3,6 +3,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 using MapsetVerifierBackend.helper;
 using MapsetParser.starrating.standard;
 
@@ -10,7 +12,28 @@ namespace MapsetVerifierBackend.renderer
 {
     class ChartRenderer : OverviewRenderer
     {
-        private static string GenerateChartData(DifficultySkillStrain diffStrain)
+        public struct Point
+        {
+            public double x { get; set; }
+            public double y { get; set; }
+        }
+        public struct Dataset
+        {
+            public string label { get; set; }
+            [JsonIgnore]
+            public string color { get; set; }
+            public string backgroundColor => color;
+            public string borderColor => color;
+            public bool fill { get; set; }
+            public List<Point> data { get; set; }
+        }
+
+        public struct ChartData
+        {
+            public List<string> labels { get; set; }
+            public List<Dataset> datasets { get; set; }
+        }
+        private static Dataset GenerateChartData(DifficultySkillStrain diffStrain)
         {
             List<string> colors = new List<string> {
                 "rgb(255, 99,  132)", // Red
@@ -21,28 +44,31 @@ namespace MapsetVerifierBackend.renderer
                 "rgb(153, 102, 255)", // Purple
                 "rgb(231, 233, 237)"  // Gray or grey
             };
+            var random = new Random();
+            int index = random.Next(colors.Count);
+            var color = colors[index];
 
-            // Start off from 0 seconds for consistent data across difficulties
-            StringBuilder sb = new StringBuilder("{x:0,y:0}");
+            List<Point> dataPoints = new List<Point>();
+            dataPoints.Add(new Point { x = 0, y = 0 });
             for (var i = 0; i < diffStrain.strainValue.Count; i++)
             {
                 var strainTime = diffStrain.strainTime[i];
                 var strainValue = Math.Round(diffStrain.strainValue[i], 2);
-                sb.Append(",{x: " + strainTime / 1000 + ", y: " + strainValue + "}");
+                dataPoints.Add(new Point { x = strainTime / 1000, y = strainValue });
             }
 
-            var random = new Random();
-            int index = random.Next(colors.Count);
-            var color = colors[index];
-            return String.Concat(
-                "{label: '", Encode(diffStrain.beatmap.metadataSettings.version), "',",
-                $"backgroundColor: '{color}', borderColor: '{color}', fill: false,",
-                $"data: [{sb.ToString()}]", "}"
-            );
+            Dataset dataset = new Dataset
+            {
+                label = Encode(diffStrain.beatmap.metadataSettings.version),
+                color = color,
+                fill = false,
+                data = dataPoints
+            };
+            return dataset;
         }
         private static string CreateChartScript(List<DifficultySkillStrain> diffStrains, string canvasId)
         {
-            var datasets = new List<string>();
+            var datasets = new List<Dataset>();
             foreach (var diff in diffStrains)
                 datasets.Add(GenerateChartData(diff));
 
@@ -50,48 +76,11 @@ namespace MapsetVerifierBackend.renderer
             foreach (var diff in diffStrains)
                 if (diff.strainTime.Count > longestX.Count)
                     longestX = diff.strainTime;
-            longestX = longestX.ConvertAll(x => x / 1000);
+            List<string> longestXStr = longestX.ConvertAll(x => (x / 1000).ToString());
 
-            var labels = String.Join("','", longestX);
-            var jsonDatasets = String.Join(',', datasets);
-
-            // This is legit atrocious to look and read.
-            // TODO: Better code to output Javascript.
-            return Script(
-                String.Concat(
-                    @"options = {
-                        spanGaps: true,
-                        responsive: true,
-                        elements: {point: {radius: 0}},
-                        tooltips: {
-                            mode: 'label',
-                        },
-                        hover: {
-                            mode: 'nearest',
-                            intersect: true
-                        },
-                        scales: {
-                        xAxes: [{
-                            display: false
-                        }],
-                        yAxes: [{
-                            display: true,
-                            scaleLabel: {
-                                display: true,
-                                labelString: 'Value'
-                            }
-                        }]
-                        }
-                    };",
-                    "data= {", $"labels: ['{labels}'],",
-                    $"datasets: [{jsonDatasets}]", "};",
-                    @"var myLineChart = new Chart('", canvasId, @"', {
-                        type: 'line',
-                        data: data,
-                        options: options
-                    });"
-                )
-            );
+            ChartData chart = new ChartData { labels = longestXStr, datasets = datasets };
+            string serealizedChart = JsonSerializer.Serialize(chart);
+            return Script($"renderChart('{canvasId}', {serealizedChart})");
         }
         private static string RenderChartCanvas(string canvasId)
         {
